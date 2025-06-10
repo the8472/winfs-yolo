ecb866df7a099e6fd5b14e7a3db8946c67597b76
The 8472 <git@infinite-source.de>
2025-06-10 23:51:25 +0200

    apply to multiple drives

--- src/main.rs
+++ src/main.rs
@@ -19,53 +19,59 @@ struct DiskCacheSettings {
 }
 
 fn main() {
-    unsafe {
-        // r#"\\?\Device\Harddisk0\DR0"#
-        // PhysicalDisk0
-        let hd1 = CString::new(r#"\\.\PHYSICALDRIVE0"#).unwrap();
-        let handle = CreateFileA(
-            hd1.as_ptr().cast(),
-            GENERIC_READ | GENERIC_WRITE,
-            FILE_SHARE_READ | FILE_SHARE_WRITE,
-            null(),
-            OPEN_EXISTING,
-            0,
-            0
-        );
+    // 3 drives ought to be enough for everyone
+    for i in 0..=2 {
+        unsafe {
+            let disk = format!(r#"\\.\PHYSICALDRIVE{}"#, i);
+            let hd1 = CString::new(disk.clone()).unwrap();
+            let handle = CreateFileA(
+                hd1.as_ptr().cast(),
+                GENERIC_READ | GENERIC_WRITE,
+                FILE_SHARE_READ | FILE_SHARE_WRITE,
+                null(),
+                OPEN_EXISTING,
+                0,
+                0
+            );
 
-        if handle == INVALID_HANDLE_VALUE {
-            let error_code = windows_sys::Win32::Foundation::GetLastError();
-            panic!("Failed to open device DR0, {:x}", error_code);
-        }
+            if handle == INVALID_HANDLE_VALUE {
+                let error_code = windows_sys::Win32::Foundation::GetLastError();
+                eprintln!("Failed to open device {disk}, {:x}", error_code);
+                continue;
+            }
 
-        let mut settings: DiskCacheSettings = mem::zeroed();
-        let mut lpbytesreturned: u32 = 0;
+            let mut settings: DiskCacheSettings = mem::zeroed();
+            let mut lpbytesreturned: u32 = 0;
 
-        let r = DeviceIoControl(handle, IOCTL_DISK_GET_CACHE_SETTINGS, null(), 0, ptr::from_mut(&mut settings).cast(), size_of::<DiskCacheSettings>() as u32, &mut lpbytesreturned, null_mut());
-        if r == 0 {
-            panic!("Failed to get cache settings");
-        }
+            let r = DeviceIoControl(handle, IOCTL_DISK_GET_CACHE_SETTINGS, null(), 0, ptr::from_mut(&mut settings).cast(), size_of::<DiskCacheSettings>() as u32, &mut lpbytesreturned, null_mut());
+            if r == 0 {
+                eprintln!("Failed to get cache settings");
+                continue;
+            }
 
-        // Enable YOLO mode
-        settings.is_power_protected = 1;
+            // Enable YOLO mode
+            settings.is_power_protected = 1;
 
-        let r = DeviceIoControl(handle, IOCTL_DISK_SET_CACHE_SETTINGS, ptr::from_ref(&settings).cast(), size_of::<DiskCacheSettings>() as u32, null_mut(), 0, &mut lpbytesreturned, null_mut());
-        if r == 0 {
-            panic!("Failed to set cache settings");
-        }
-        
-        // verify that it sticks
+            let r = DeviceIoControl(handle, IOCTL_DISK_SET_CACHE_SETTINGS, ptr::from_ref(&settings).cast(), size_of::<DiskCacheSettings>() as u32, null_mut(), 0, &mut lpbytesreturned, null_mut());
+            if r == 0 {
+                eprintln!("Failed to set cache settings");
+                continue;
+            }
 
-        let r = DeviceIoControl(handle, IOCTL_DISK_GET_CACHE_SETTINGS, null(), 0, ptr::from_mut(&mut settings).cast(), size_of::<DiskCacheSettings>() as u32, &mut lpbytesreturned, null_mut());
+            // verify that it sticks
 
-        if r == 0 {
-            panic!("Failed to get cache settings after setting");
-        }
+            let r = DeviceIoControl(handle, IOCTL_DISK_GET_CACHE_SETTINGS, null(), 0, ptr::from_mut(&mut settings).cast(), size_of::<DiskCacheSettings>() as u32, &mut lpbytesreturned, null_mut());
 
-        if settings.is_power_protected != 1 {
-            panic!("Failed to enable YOLO mode, is_power_protected is not set");
-        } else {
-            println!("YOLO mode enabled successfully!");
+            if r == 0 {
+                eprintln!("Failed to get cache settings the second time");
+                continue;
+            }
+
+            if settings.is_power_protected != 1 {
+                eprintln!("Failed to enable YOLO mode for drive {i}, is_power_protected was not retained");
+            } else {
+                eprintln!("YOLO mode enabled successfully for drive {i}!");
+            }
         }
     }
 }
